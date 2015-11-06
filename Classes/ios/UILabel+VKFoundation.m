0a46625a44700aeb057dff9fc536849032c961da
Luu Gia Thuy <luugiathuy@gmail.com>
2015-11-06 10:51:30 +0800

    Fix sizeWithFont deprecated method
    Replace by sizeWithAttributes and boundingRectWithSize

--- Classes/ios/UILabel+VKFoundation.m
+++ Classes/ios/UILabel+VKFoundation.m
@@ -8,27 +8,39 @@
 @implementation UILabel (VKFoundation)
 
 - (void)alignTop {
-    CGSize fontSize = [self.text sizeWithFont:self.font];
-    double finalHeight = fontSize.height * self.numberOfLines;
-    double finalWidth = self.frame.size.width;    //expected width of label
-    CGSize theStringSize = [self.text sizeWithFont:self.font constrainedToSize:CGSizeMake(finalWidth, finalHeight) lineBreakMode:self.lineBreakMode];
-    int newLinesToPad = (finalHeight  - theStringSize.height) / fontSize.height;
-    for(int i=0; i<newLinesToPad; i++)
-        self.text = [self.text stringByAppendingString:@"\n "];
+  CGSize fontSize = [self.text sizeWithAttributes:@{NSFontAttributeName: self.font}];
+  CGFloat finalHeight = fontSize.height * self.numberOfLines;
+  CGFloat finalWidth = self.frame.size.width;    //expected width of label
+  CGSize theStringSize = CGRectIntegral([self boundingRectWithSize:CGSizeMake(finalWidth, finalHeight)]).size;
+  int newLinesToPad = (finalHeight  - theStringSize.height) / fontSize.height;
+  for (int i = 0; i < newLinesToPad; i++) {
+    self.text = [self.text stringByAppendingString:@"\n "];
+  }
 }
 
 - (void)alignBottom {
-    CGSize fontSize = [self.text sizeWithFont:self.font];
-    double finalHeight = fontSize.height * self.numberOfLines;
-    double finalWidth = self.frame.size.width;    //expected width of label
-    CGSize theStringSize = [self.text sizeWithFont:self.font constrainedToSize:CGSizeMake(finalWidth, finalHeight) lineBreakMode:self.lineBreakMode];
-    int newLinesToPad = (finalHeight  - theStringSize.height) / fontSize.height;
-    for(int i=0; i<newLinesToPad; i++)
-        self.text = [NSString stringWithFormat:@" \n%@",self.text];
+  CGSize fontSize = [self.text sizeWithAttributes:@{NSFontAttributeName: self.font}];
+  CGFloat finalHeight = fontSize.height * self.numberOfLines;
+  CGFloat finalWidth = self.frame.size.width;    //expected width of label
+  CGSize theStringSize = CGRectIntegral([self boundingRectWithSize:CGSizeMake(finalWidth, finalHeight)]).size;
+  int newLinesToPad = (finalHeight  - theStringSize.height) / fontSize.height;
+  for(int i = 0; i < newLinesToPad; i++) {
+    self.text = [NSString stringWithFormat:@" \n%@",self.text];
+  }
 }
 
 - (void)sizeToFitMaxSize:(CGSize)size {
-  CGSize targetSize = [self.text sizeWithFont:self.font constrainedToSize:size lineBreakMode:NSLineBreakByTruncatingTail];
+  NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+  paragraphStyle.lineBreakMode = NSLineBreakByTruncatingTail;
+  paragraphStyle.alignment = self.textAlignment;
+  CGRect targetRect = [self.text boundingRectWithSize:size
+                                              options:(NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading)
+                                           attributes:@{
+                                                        NSFontAttributeName: self.font,
+                                                        NSParagraphStyleAttributeName : paragraphStyle,
+                                                        }
+                                              context:nil];
+  CGSize targetSize = CGRectIntegral(targetRect).size;
   self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, targetSize.width, targetSize.height);
 }
 
@@ -38,5 +50,20 @@
 
 - (void)sizeToFitMaxLines:(NSInteger)lines {
   [self sizeToFitMaxHeight:lines * self.font.lineHeight];
+}
+
+#pragma mark - Private
+- (CGRect)boundingRectWithSize:(CGSize)size {
+  NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+  paragraphStyle.lineBreakMode = self.lineBreakMode;
+  paragraphStyle.alignment = self.textAlignment;
+  CGRect theStringRect = [self.text boundingRectWithSize:size
+                                                 options:NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading
+                                              attributes:@{
+                                                           NSFontAttributeName: self.font,
+                                                           NSParagraphStyleAttributeName : paragraphStyle,
+                                                           }
+                                                 context:nil];
+  return theStringRect;
 }
 @end
